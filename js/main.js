f02f52716d66d77518c99d2eeb7aeba34dfdef32
DGrazz <mariogalo2008@gmail.com>
2023-11-09 14:56:09 +0100

    introducir ciudad para buscar

--- js/main.js
+++ js/main.js
@@ -1,28 +1,56 @@
 const { createApp } = Vue
 
-import { getTiempo } from "./communicationManager.js";
+// import { getTiempo } from "./communicationManager.js";
 
 createApp({
     data() {
         return {
-            location: [],
-            current: [],
+            cityName: "",
+            location: {},
+            current: {},
         }
     },
     methods: {
 
-        
+        searchCity() {
+            
+            if (this.cityName) {
+                this.fetchCity(this.cityName);
+            }else{
+                console.log("No hay ciudades para buscar");
+            }
+        },
+
+        async fetchCity() {
+            try {
+                const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${this.cityName}`;
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        'X-RapidAPI-Key': 'a8765e05femsha073d3056235332p1971b0jsnf31f19a1e1e4',
+                        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+                    }
+                }
+                const response = await fetch(url, options);
+                const result = await response.json();
+                this.location = result.location;
+                this.current = result.current;
+            } catch (error) {
+                console.error(error);
+            }
+        },
+
     },
     created() {
-        getTiempo().then(data => {
-            this.location = data.location;
-            this.current = data.current;
-            console.log(this.location);
-            console.log(this.current);
-        });
+        // getTiempo().then(data => {
+        //     this.location = data.location;
+        //     this.current = data.current;
+        //     console.log(this.location);
+        //     console.log(this.current);
+        // });
     }
 
-   
+
 
 
 }).mount('#app')
